8454ad1ff32e215c759e44270ec08f73dee306db
Sunil Pai <threepointone@gmail.com>
2015-05-31 13:45:22 +0530

    .

--- src/parse.js
+++ src/parse.js
@@ -8,18 +8,31 @@ import estemplate from 'estemplate';
 export default function transform(src){
   return escodegen.generate(estraverse.replace(acorn.parse(src), {
     leave: function (node, parent) {
-        if (node.type === 'CallExpression' && node.callee.name === 'register'){
-          return {
-            ...node,
-            callee: {
-              ...node.callee,
-              name: 'require("../../disto-hot-loader/lib/decorate").register(register, module)'
-            }
-          };
-        }
-        if (node.type === 'CallExpression' && (node.callee.property || {}).name === 'register'){
-          return estemplate('require("../../disto-hot-loader/lib/decorate").register (<%= callee %>, module)((%= arguments %))', node);
-        }
+      if (node.type === 'CallExpression' && node.callee.name === 'register'){
+        return {
+          ...node,
+          callee: {
+            ...node.callee,
+            name: 'require("disto-hot-loader/lib/decorate").register(register, module)'
+          }
+        };
+      }
+      if (node.type === 'CallExpression' && (node.callee.property || {}).name === 'register'){
+        return estemplate('require("disto-hot-loader/lib/decorate").register(<%= callee %>, module)((%= arguments %))', node);
+      }
+
+      if (node.type === 'CallExpression' && node.callee.name === 'act'){
+        return {
+          ...node,
+          callee: {
+            ...node.callee,
+            name: 'require("disto-hot-loader/lib/decorate").act(register, module)'
+          }
+        };
+      }
+      if (node.type === 'CallExpression' && (node.callee.property || {}).name === 'act'){
+        return estemplate('require("disto-hot-loader/lib/decorate").act(<%= callee %>, module)((%= arguments %))', node);
+      }
     }
   }));
 }
